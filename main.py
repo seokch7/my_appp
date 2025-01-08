1106d413167f0d09f822c2ec7e34e6b30f665dda
seokch7 <24_10924@daejin.sen.hs.kr>
2025-01-08 11:22:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ st.write("저의 이메일 주소는 seokch7@naver.com 입니다.")
 
 st.button("Reset", type="primary")
 if st.button("난수 생성"):
-    st.write(random.randint(1,1000)
+    st.write(random.randint(1,1000))
 else:
     st.write("안녕")

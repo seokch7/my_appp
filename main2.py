5e1e998db5af602ef133661ff6e44733ef4db6f2
seokch7 <24_10924@daejin.sen.hs.kr>
2025-01-08 11:37:08 +0900

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import random
+
+st.title("나의 위키 앱")
+
+st.text("\n\n")
+
+
+
+whie True:
+  검색 = input("")
+  if 검색==stop:
+    break
+  
+  st.write(f"나무위키:https://namu.wiki/w/{검색}")
+  st.write(f"나무위키:https://ko.wikipedia.org/wiki/{검색}")
+  
+  

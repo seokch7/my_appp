94dec3ff45ad7b3bd250a2c592114775a32a9746
seokch7 <24_10924@daejin.sen.hs.kr>
2025-01-08 12:07:11 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from openai import OpenAI
-API_key = st.text_input("api key:")
-client = OpenAI(api_key = API_key)
+
 st.title("나의 위키 앱")
 
 st.text("\n\n")
@@ -14,15 +12,8 @@ while True:
   a = st.text_input("무엇을 찾고 싶나요?:")
 
 
-  completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-      {"role": "developer", "content": "You are a helpful assistant."},
-      {"role": "user", "content": a }
-    ]
-  )
+  
 
-  print(completion.choices[0].message.content)
 
   
   st.link_button("Go to 나무위키", f"https://namu.wiki/w/{a}")
